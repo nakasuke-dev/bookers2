5c82490414ed9b0e955ce646cbc1ee4774d1c806
yusuke nakano <nakasuke0509.biz@gmail.com>
2021-05-23 21:07:53 +0900

    feat: ユーザー一覧画面・編集画面を作成

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '5d598b869a08c7a509c9730f2fbb74554dbb08cccbf12a20b8211c797b9ebba89bc6400c4d662e3394daec48c885fae9909305c885fafdc7ca990d8ea64c862d'
