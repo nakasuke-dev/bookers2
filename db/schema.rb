fde3b470b77168379e7961abf238fb74acbb82c5
yusuke nakano <nakasuke0509.biz@gmail.com>
2021-05-24 01:37:31 +0900

    fix: bookのopinionカラムをbodyに変更し、バリデーションを実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_23_120826) do
+ActiveRecord::Schema.define(version: 2021_05_23_163206) do
 
   create_table "books", force: :cascade do |t|
     t.string "title", default: "", null: false
-    t.string "opinion", default: "", null: false
+    t.string "body", default: "", null: false
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
