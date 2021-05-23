54d43a1d78da27e7fc0814e9cff1cd18197ed6a2
yusuke nakano <nakasuke0509.biz@gmail.com>
2021-05-23 23:07:48 +0900

    feat: 本のcrudコントローラーとrouting

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get '/about', to: 'homes#about', as: 'about'
   resources :users,  only: [:index, :show, :edit, :update]
+  resources :books,  except: [:new]
 end
